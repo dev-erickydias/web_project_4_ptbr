e7a58fff182d93908b05a9b1ab853b400a46ffa8
dev-erickydias <erickyhenriquesd@gmail.com>
2023-08-16 15:42:32 +0100

    acabei de fazer as interações do botao edit fiz html tambem diz o css do inicio do perfil esta faltando as interacoes do botao add tb falta o html e css, add as fontes e algumas imagens para os campos que ja foram feitos

---
+++ src/pages/index.js
@@ -0,0 +1,30 @@
+//Aqui é a Programação do Button de edição do perfil.
+
+//Parte de fazer o pupup abrir, alem de fazer tudo o que for editado ficar na tela.
+const profileButton = document.querySelector('.profile__button');
+const profileForm = document.querySelector('.form__edit');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
+const inputNome = document.querySelector('.nome');
+const inputJob = document.querySelector('.job');
+const formClose = document.querySelector('.form__close')
+const submitForm = document.querySelector('.submit__button')
+
+profileButton.addEventListener('click', () => {
+  profileForm.style.display = 'block';
+});
+profileForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+  profileTitle.textContent = inputNome.value;
+  profileSubtitle.textContent = inputJob.value;
+});
+
+// Aqui irei programar para ele fechar com click fora do pupup, no X e no submit 
+
+formClose.addEventListener('click', () => {
+  profileForm.style.display = 'none'
+})
+
+submitForm.addEventListener('click', () => {
+  profileForm.style.display = 'none'
+})
